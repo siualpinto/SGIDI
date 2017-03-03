b2ddafdc0d51dd70a96692a34a9936800436c9b5
siualpinto <lsmoreira_77@hotmail.com>
2017-03-03 12:21:07 +0000

    Ideias template v1.7  avaliação de ideias - falta pedido ajax para o estado e backend da avaliação de ideias ✔  95% frontend done ✔

--- sgidi/static/js/base/base.js
+++ sgidi/static/js/base/base.js
@@ -1,12 +1,17 @@
 /**
  * Created by lsmor on 20/02/2017.
  */
-function blinker() {
-    $('.blink_me').fadeOut(500);
-    $('.blink_me').fadeIn(500);
+var  blink = $('.blink_me');
+function blinker()
+{
+    blink.fadeOut(500);
+    blink.fadeIn(500);
 }
-if($('.blink_me').text() > 0) {
+if(blink.text() > 0)
+{
     setInterval(blinker, 2000);
-}else{
-    $('.blink_me').remove();
+}
+else
+{
+    blink.remove();
 }
--- sgidi/static/js/ideias.js
+++ sgidi/static/js/ideias.js
@@ -1,22 +1,48 @@
-
-
+/*
+* Modificação do estado do radio button para a escolha de outro tipo de ideia
+*
+* */
 $('input[type="radio"]').change(function () {
+    var outra_text = $('#radio_outra_text');
     if ($('#radio_outra').is(':checked'))
     {
-        $('#radio_outra_text').prop('disabled',false);
-        $('#radio_outra_text').prop('required',true);
+        outra_text.prop('disabled',false);
+        outra_text.prop('required',true);
     }
     else
     {
-        $('#radio_outra_text').prop('disabled',true);
-        $('#radio_outra_text').prop('required',false);
+        outra_text.prop('disabled',true);
+        outra_text.prop('required',false);
     }
 });
 
+/*
+* Adicionar uma linha, depois da penultima linha da tabela de analise
+* Novas linhas são editáveis
+* */
 $(".add_line").click(function () {
-    $('#tabela_avaliacao').find('> tbody:last-child').append('<tr><td><div contenteditable>Nova linha</div></td><td></td><td></td><td></td> <td></td><td></td><td></td></tr>');
+    $('#tabela_avaliacao').find('tr:nth-last-child(2)').after('<tr><td><div contenteditable>Nova linha</div></td><td></td><td></td><td></td> <td></td><td></td><td></td></tr>');
 });
 
+/*
+* Eliminar uma linha, depois da penultima linha da tabela de analise
+*
+* */
+$(".delete_line").click(function () {
+    //$('#tabela_avaliacao').find('> tbody:last-child').remove();
+    var tabela = $('#tabela_avaliacao');
+    if(tabela.find('> tbody:last-child').children().length != 10)
+    {
+        console.log(tabela.find('> tbody:last-child').prev().children().last());
+        tabela.find('> tbody:last-child').children().last().prev().remove();
+    }
+
+});
+
+/*
+* O utilizador pode inserir uma cruz na avaliação desejada (muito fraco, ..., Muito Bom) para cada linha
+*
+* */
 $("#tabela_avaliacao").on("click", "td:not(:first-child, :last-child)", function() {
     if($(this).siblings().children().is("i"))
     {
@@ -27,3 +53,7 @@ $("#tabela_avaliacao").on("click", "td:not(:first-child, :last-child)", function
         $(this).html("<i class='fa fa-times smallicon'></i>");
     }
 });
+
+ $("#id_username").change(function () {
+      console.log( $(this).val() );
+    });
